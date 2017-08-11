943e2d90f98583ca5156776e4d9679a72a72e87e
Andrew Dicer <andrew.dicer@gmail.com>
2017-08-11 19:43:13 -0400

    added JAWSBD

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "TODOList_db"
-});
+if (process.evn.JAWSBD_URL) {
+  connection = mysql.createConnection(PROCESS.INV.JAWSBD_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "TODOList_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
